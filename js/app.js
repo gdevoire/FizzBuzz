46a35239b69e64f059baf02e8115a681091eb224
gdevoire <gabe.devoire@gmail.com>
2016-01-20 11:18:32 -0500

    html update

--- js/app.js
+++ js/app.js
@@ -3,18 +3,18 @@ $(document).ready(function() {
   for ( i = 1; i <= 100; i++) {
 
     if ( i % 15 === 0) {
-      $( "span").append("Fizzbuzz");
+      $( "li").append("Fizzbuzz");
     }
     else if ( i % 3 === 0) {
-      $( "span").append("Fizz");
+      $( "li").append("Fizz");
     }
 
     else if ( i % 5 === 0 ) {
-      $( "span").append("Buzz");
+      $( "li").append("Buzz");
     }
 
     else {
-      $( "span").append(i);
+      $( "li").append(i);
     }
 
   }
